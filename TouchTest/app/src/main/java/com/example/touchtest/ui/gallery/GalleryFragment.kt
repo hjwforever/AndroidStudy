ff4852df6cd914786d7c9a21169d6d0e4b0b3461
hjwforever <1326153330@qq.com>
2020-12-28 15:39:10 +0800

    init commit of TouchTest

---
+++ TouchTest/app/src/main/java/com/example/touchtest/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,35 @@
+package com.example.touchtest.ui.gallery
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.touchtest.R
+import com.example.touchtest.databinding.FragmentGalleryBinding
+
+class GalleryFragment : Fragment() {
+
+    private lateinit var galleryViewModel: GalleryViewModel
+
+    private var _binding: FragmentGalleryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        galleryViewModel =
+                ViewModelProvider(this).get(GalleryViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_gallery, container, false)
+        val textView: TextView = root.findViewById(R.id.text_gallery)
+        galleryViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
